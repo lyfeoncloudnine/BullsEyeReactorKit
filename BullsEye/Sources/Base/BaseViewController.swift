4c46139863f17abca98e7e7d0cf37a6f97848085
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-04-28 18:29:33 +0900

    ✨ Add BaseViewController

---
+++ BullsEye/Sources/Base/BaseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  BullsEye
+//
+//  Created by lyfeoncloudnine on 2023/04/28.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    func configureViews() {
+        // Override Point
+    }
+}
