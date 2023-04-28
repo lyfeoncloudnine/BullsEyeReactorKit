7c78cd3fa87e376c130cde3543ab0d0244df3190
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-04-28 16:48:20 +0900

    🚧 Add RecordServiceType

---
+++ BullsEye/Sources/Services/RecordService/RecordServiceType.swift
@@ -0,0 +1,15 @@
+//
+//  RecordServiceType.swift
+//  BullsEye
+//
+//  Created by lyfeoncloudnine on 2023/04/28.
+//
+
+import Foundation
+
+protocol RecordServiceType {
+    func records() -> [Record]
+    func create(record: Record) -> [Record]
+    func delete(record: Record) -> [Record]
+    func clear()
+}
