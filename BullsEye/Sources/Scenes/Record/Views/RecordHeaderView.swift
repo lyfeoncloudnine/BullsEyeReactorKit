18a7d2fa612af6e018798f207f1f6cc5c64122a1
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-05-02 17:45:04 +0900

    🚧 Add RecordHeaderView

---
+++ BullsEye/Sources/Scenes/Record/Views/RecordHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  RecordHeaderView.swift
+//  BullsEye
+//
+//  Created by lyfeoncloudnine on 2023/05/02.
+//
+
+import UIKit
+
+class RecordHeaderView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
