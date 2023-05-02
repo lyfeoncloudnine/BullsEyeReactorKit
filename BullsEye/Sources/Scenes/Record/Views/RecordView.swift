159d809a1b90a671a9508c62597367b6cb476156
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-05-02 15:25:59 +0900

    ✨ Implement RecordView

--- BullsEye/Sources/Scenes/Record/Views/RecordView.swift
+++ BullsEye/Sources/Scenes/Record/Views/RecordView.swift
@@ -8,5 +8,20 @@
 import UIKit
 
 final class RecordView: BaseView {
+    let editButton = UIButton(type: .system).then {
+        $0.setImage(UIImage(systemName: "pencil"), for: .normal)
+    }
     
+    let tableView = UITableView().then {
+        $0.register(RecordTableViewCell.self, forCellReuseIdentifier: RecordTableViewCell.reuseIdentifier)
+        $0.separatorStyle = .none
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+        
+        addSubviews(tableView)
+        
+        tableView.hook.all(to: self, safeAreaSides: [.top])
+    }
 }
