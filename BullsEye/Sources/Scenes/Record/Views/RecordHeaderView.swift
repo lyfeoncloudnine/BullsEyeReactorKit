68b99617a2fd10984df9e0f390c895968a911c3b
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-05-02 17:55:58 +0900

    ✨ Add RecordHeaderView

--- BullsEye/Sources/Scenes/Record/Views/RecordHeaderView.swift
+++ BullsEye/Sources/Scenes/Record/Views/RecordHeaderView.swift
@@ -7,14 +7,33 @@
 
 import UIKit
 
-class RecordHeaderView: BaseView {
+import Hook
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class RecordHeaderView: BaseView {
+    private let targetNumberDescLabel = UILabel().then {
+        $0.text = "맞춘 숫자"
+        $0.font = .preferredFont(forTextStyle: .title3)
     }
-    */
-
+    
+    private let scoreDescLabel = UILabel().then {
+        $0.text = "점수"
+        $0.font = .preferredFont(forTextStyle: .title3)
+    }
+    
+    override func configureViews() {
+        backgroundColor = .lightGray
+        
+        addSubviews(targetNumberDescLabel, scoreDescLabel)
+        
+        targetNumberDescLabel.hook
+            .top(equalTo: topAnchor, constant: 8)
+            .leading(equalTo: leadingAnchor, constant: 8)
+            .bottom(equalTo: bottomAnchor, constant: -8)
+        
+        scoreDescLabel.hook
+            .top(equalTo: targetNumberDescLabel.topAnchor)
+            .trailing(equalTo: trailingAnchor, constant: -8)
+            .bottom(equalTo: targetNumberDescLabel.bottomAnchor)
+    }
 }
--- BullsEye/Sources/Scenes/Record/Views/RecordView.swift
+++ BullsEye/Sources/Scenes/Record/Views/RecordView.swift
@@ -8,8 +8,11 @@
 import UIKit
 
 final class RecordView: BaseView {
-    let tableView = UITableView().then {
+    let headerView = RecordHeaderView()
+    
+    lazy var tableView = UITableView(frame: .zero, style: .grouped).then {
         $0.register(RecordTableViewCell.self, forCellReuseIdentifier: RecordTableViewCell.reuseIdentifier)
+        $0.delegate = self
     }
     
     override func configureViews() {
@@ -18,5 +21,15 @@ final class RecordView: BaseView {
         addSubviews(tableView)
         
         tableView.hook.all(to: self, safeAreaSides: [.top])
+    }
+}
+
+extension RecordView: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        systemLayoutSizeFitting(CGSize(width: tableView.frame.width, height: UIView.layoutFittingCompressedSize.height)).height
     }
 }
