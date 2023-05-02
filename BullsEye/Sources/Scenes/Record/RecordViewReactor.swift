a191c687cbb21cd1d85edecf651057d59f7921b7
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-05-02 15:27:50 +0900

    🚧 Add RecordViewReactor

---
+++ BullsEye/Sources/Scenes/Record/RecordViewReactor.swift
@@ -0,0 +1,20 @@
+//
+//  RecordViewReactor.swift
+//  BullsEye
+//
+//  Created by lyfeoncloudnine on 2023/05/02.
+//
+
+import Foundation
+
+import ReactorKit
+
+final class RecordViewReactor: Reactor {
+    enum Action {}
+    
+    enum Mutation {}
+    
+    struct State {}
+    
+    let initialState = State()
+}
